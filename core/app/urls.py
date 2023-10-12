5e49f564eed0b4c213ecbf60c025cd10e14dbf9b
timurrrrrrr <djavarbekovt@gmail.com>
2023-10-12 19:44:02 +0600

    Podcorictiroval kod

--- core/app/urls.py
+++ core/app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.category_list, name='category-list'),
-    path('products/', views.product_list, name='product-list'),
-    path('brands/', views.brand_list, name='brand-list'),
-    path('products/<int:product_id>/', views.product_detail, name='product-detail'),
+    path('categories/', views.CategoryList.as_view()),
+    path('products/', views.ProductList.as_view()),
+    path('brands/', views.BrandList.as_view()),
+    path('products/<int:product_id>/', views.product_detail),
 ]
